75e4b78620a95a45d359adc97822b5819c21c839
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-05 23:18:24 +0530

    intro to js

---
+++ array.js
@@ -0,0 +1,24 @@
+// empty array 
+let arr = [];
+console.log(arr);
+
+//array with elements 
+ let elearr = [ 1,2,3,4,'iam string',false, 4.5];
+ console.log(elearr);
+
+ console.log("element at 4 the index " + elearr[4]);
+ console.log("element at 1 the index " + elearr[0]);
+ 
+
+ // elements change in array
+ elearr[3]='nothing';
+
+ console.log("######################");
+
+
+//  array methods
+// 1. push
+ 
+console.log("array befor push: "+elearr);
+elearr.push("yeah");
+console.log("array after push: "+elearr);
