3f8739d414bb82c64efab9dda627793b04dc1282
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-08 22:16:52 +0530

    objects and thn objects in objects also function in object's key

--- object.js
+++ object.js
@@ -1,12 +1,29 @@
-// empty object
-let obj={};
-console.log(obj);
+// // empty object
+// let obj={};
+// console.log(obj);
 
-let objdeepak = {
-    "name": 'Deepak',
-    "age": '21',
-    phoneno: '987456321',
-    lastname: 'sharma'
-};
+// // objects with some content
+// let objdeepak = {
+//     "name": 'Deepak',
+//     "age": '21',
+//     phoneno: '987456321',
+//     lastname: 'sharma'
+// };
 
-console.log(objdeepak);
+// console.log(objdeepak);
+
+// captianamerica objects
+let capamerica= {
+    Name : 'steve',
+    Age: 9999,
+    friends: ['natasha','thor','tony','bucci'],
+    Address:{
+        City: 'queens',
+        state: 'haryana',
+    },
+    sayHI: function(){
+        console.log("cap america say hello");
+    }
+
+}
+console.log(capamerica);
