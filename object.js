cd23d2c5e864a18318e454ff222a8ebba75b3e65
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-08 23:14:37 +0530

    chnge and acces in different ways

--- object.js
+++ object.js
@@ -21,6 +21,7 @@ let capamerica= {
         City: 'queens',
         state: 'haryana',
     },
+    isavenger:false,
     sayHI: function(){
         console.log("cap america say hello");
     }
@@ -37,6 +38,26 @@ console.log(capamerica.Address.City);
 console.log(capamerica.friends[0]);
 // function k liye 
 console.log(capamerica.sayHI()); // yeh undefined isloye aaya bcz no return value
-
-
 capamerica.sayHI();
+
+console.log("object before update",capamerica)
+// add a new key to objects
+capamerica.Movies = ['avengers','civilwar','endsame']
+// after updation
+console.log("object after updation",capamerica)
+
+// to delete jkey
+delete capamerica.Movies;
+console.log("after deletion",capamerica)
+// change key
+capamerica.isavenger=true;
+console.log("after chnage of false to true",capamerica);
+capamerica.Address.state= 'las vegas';
+console.log("chnage of city",capamerica);
+
+// access a key another way to access 
+
+// 1. acess key
+console.log(capamerica.Name);
+// 2. tareeka
+console.log(capamerica['Name']);
