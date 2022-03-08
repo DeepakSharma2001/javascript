053c336bb2e65c1d62de71c8db72955b5312febd
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-09 00:02:10 +0530

    intro to js

---
+++ node/childprocess.js
@@ -0,0 +1,6 @@
+let cp = require('child_process'); // yeh btara hai hm childprocess use krre hai
+// console.log(cp);
+
+//open calculator with help of this file 
+
+cp.execFileSync("calc"); // for linux gnome-calculator
