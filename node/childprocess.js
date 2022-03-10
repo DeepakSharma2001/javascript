114cac7e4e95d25ce48c9d90e695998167f32ac6
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-10 22:11:23 +0530

    path added

--- node/childprocess.js
+++ node/childprocess.js
@@ -7,12 +7,8 @@ let cp = require('child_process'); // yeh btara hai hm childprocess use krre hai
 // open calculator
 // cp.execFileSync("calc"); // for linux gnome-calculator
 
-// // open vs code 
-// cp.execSync("code");
-
-let content = cp.execSync("node test.js"); // execute different files 
-
-console.log("output of test.js "+ content);
+// open vs code 
+cp.execSync("code");
 
 
 
---
+++ node/path.js
@@ -0,0 +1,19 @@
+// "C:\Users\DEEPAK\Desktop\FJP-6\module2\node\path.js"
+
+// to give path 
+ let path = require("path");
+//  console.log(path);
+
+// extension name  batat hai yeh 
+// let extensionname = path.extname("C:\Users\DEEPAK\Desktop\FJP-6\module2\node\path.js");
+// console.log(extensionname);
+
+// base name
+let baseName = path.basename(__filename);
+console.log(baseName);
+
+// __dir name  ( gives us directory path )
+console.log(__dirname);
+
+// __filename (gives file path)
+console.log(__filename);
