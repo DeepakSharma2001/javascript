0c291384a1c73f6b80e9546e493ab57aed77eb42
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-10 23:36:36 +0530

    CRUD

--- node/path.js
+++ node/path.js
@@ -8,12 +8,20 @@
 // let extensionname = path.extname("C:\Users\DEEPAK\Desktop\FJP-6\module2\node\path.js");
 // console.log(extensionname);
 
-// base name
-let baseName = path.basename(__filename);
-console.log(baseName);
+// // base name
+// let baseName = path.basename(__filename);
+// console.log(baseName);
 
 // __dir name  ( gives us directory path )
 console.log(__dirname);
 
 // __filename (gives file path)
 console.log(__filename);
+
+
+let dirpath =  __dirname;
+console.log(dirpath);
+
+let newfilepath = path.join(dirpath,"test.js");
+console.log(newfilepath);
+
