b6cd17e293b854c9b0b690ac06bb5ff8ec602212
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-10 21:48:26 +0530

    child process in which we execute file

--- node/childprocess.js
+++ node/childprocess.js
@@ -3,4 +3,19 @@ let cp = require('child_process'); // yeh btara hai hm childprocess use krre hai
 
 //open calculator with help of this file 
 
-cp.execFileSync("calc"); // for linux gnome-calculator
+
+// open calculator
+// cp.execFileSync("calc"); // for linux gnome-calculator
+
+// // open vs code 
+// cp.execSync("code");
+
+let content = cp.execSync("node test.js"); // execute different files 
+
+console.log("output of test.js "+ content);
+
+
+
+
+
+
