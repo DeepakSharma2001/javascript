1b5ac2f71877bd03ae47d74d3e9ce0152f6fca6a
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-15 22:53:12 +0530

    question 3

---
+++ node/questions/q3.js
@@ -0,0 +1,15 @@
+// copy index.html from module 1 to  insidde module 2 having name html 
+
+// double dot .. ek dir peeche le jata hai
+
+
+let fs = require("fs");
+let path = require("path");
+
+let srcpath = path.join(__dirname,"..","..","..","Module1","index.html")
+console.log("source path --->", srcpath);
+let destpath = path.join(__dirname,"..","..",'index.html');
+console.log("destination--->", destpath);
+
+
+fs.copyFileSync(srcpath,destpath);
