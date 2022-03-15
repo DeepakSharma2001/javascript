999efd5beb9a28f7e4d8a38b085f67743a11e1d2
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-15 22:26:50 +0530

    question 2 complted

--- node/questions/q2.js
+++ node/questions/q2.js
@@ -4,15 +4,26 @@
 let fs = require("fs");
 let path = require("path");
 
-let arr = ['audio','video','image','software','documents','aplication','other'];
- 
+let arr = ['audio','video','software','documents','aplication','other'];
+let nameArr = ['abc','afg','xyz','def']; 
+let extArr = ['.mp3','.mp4','.exe','.pdf','.apk','.rar'];
+
 let organisedPath = path.join(__dirname,"organised");
 if(!fs.existsSync(organisedPath)){
 fs.mkdirSync(organisedPath)
 }
+
 for(let i =0; i<arr.length;i++){
     let folderKaPath = path.join(organisedPath,arr[i]);
-    fs.mkdirSync(folderKaPath);
+    if (!fs.existsSync(folderKaPath)){
+        fs.mkdirSync(folderKaPath);
+    }    
+    for(let j = 0; j<nameArr.length;j++){
+        let fileName = nameArr[j]+extArr[i];
+        let filekapath = path.join(folderKaPath,fileName);
+        fs.writeFileSync(filekapath,"");
+
+    }
 }
 
 
