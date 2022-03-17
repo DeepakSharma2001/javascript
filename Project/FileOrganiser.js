ce7646a3369f70d90aa50e1db927675c174c47ce
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-17 21:35:27 +0530

    project FileOrganiser

---
+++ Project/FileOrganiser.js
@@ -0,0 +1,6 @@
+// console.log("hellowrld")
+let inputArr = process.argv;  // this will give output in array 
+console.log(inputArr);
+//['pathtonode','pathtofile','input_paseed_in_space']
+let input = inputArr[6];
+console.log(input);
