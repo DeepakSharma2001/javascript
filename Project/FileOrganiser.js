365524e14f5127fb30c1ab684bd90d5a787d3492
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-17 22:34:25 +0530

    extension along with folder name added

--- Project/FileOrganiser.js
+++ Project/FileOrganiser.js
@@ -1,15 +1,44 @@
 let fs = require("fs");
-let FolderPath = process.argv[2]; // input ka tareeka
+let path = require("path");
+let FolderPath = process.argv[2]; // input ka tareeka  another option path.join(__dirname,"Download");  if error comes
 
 // console.log(FolderPath);
  let folderExist = fs.existsSync(FolderPath);
+
+ let extensions = {
+    Audio: [".mp3"],
+    Video: [".mp4",".mkv",],
+    Document: [".doc",".pdf",".xlsx",".txt"],
+    Image: [ ".jpeg",".jpg",".gif"],
+    Software: [".exe"]
+};
 
  if(folderExist){
      // we will code here
     //  console.log("path is valid!!!"); 
     let files = fs.readdirSync(FolderPath);
-    console.log(files);
+    for(let i=1; i<files.length;i++){
+        let ext = path.extname(files[i]);
+        let NameOfFolder = giveFolderName(ext); 
+        console.log("Ext--->",ext,"Folder--->",NameOfFolder);
+    }
+    // console.log(files);
     }
  else{
      console.log("please enter a Valid Path!!!");
  }
+
+
+
+ function giveFolderName(ext){
+     for(let key in extensions){   // key according to our object  // for each loop hai yeah
+         let extArr = extensions[key];
+         for(let i=0; i<extArr.length; i++){
+             if(extArr[i]==ext){
+                 return key;
+            }
+        }
+
+    }
+     return 'Others';
+}
