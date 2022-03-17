923fac56a9ef6a07758c5814cf29e6b2e748e4d4
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-17 23:32:53 +0530

    project completed

--- Project/FileOrganiser.js
+++ Project/FileOrganiser.js
@@ -1,44 +1,66 @@
 let fs = require("fs");
-let path = require("path");
-let FolderPath = process.argv[2]; // input ka tareeka  another option path.join(__dirname,"Download");  if error comes
+	let path = require("path");
+	let folderPath = process.argv[2];
+	
 
-// console.log(FolderPath);
- let folderExist = fs.existsSync(FolderPath);
+	// console.log(folderPath);
+	let folderExists = fs.existsSync(folderPath);
+	
 
- let extensions = {
-    Audio: [".mp3"],
-    Video: [".mp4",".mkv",],
-    Document: [".doc",".pdf",".xlsx",".txt"],
-    Image: [ ".jpeg",".jpg",".gif"],
-    Software: [".exe"]
-};
+	let extensions = {
+	    Audio:[".mp3"],
+	    Video:[".mp4",".mkv"],
+	    Document:[".doc",".xlsx",".pdf",".txt"],
+	    Image:[".jpeg",".jpg",".png",".gif"],
+	    Software:[".exe"]
+	};
+	
 
- if(folderExist){
-     // we will code here
-    //  console.log("path is valid!!!"); 
-    let files = fs.readdirSync(FolderPath);
-    for(let i=1; i<files.length;i++){
-        let ext = path.extname(files[i]);
-        let NameOfFolder = giveFolderName(ext); 
-        console.log("Ext--->",ext,"Folder--->",NameOfFolder);
-    }
-    // console.log(files);
-    }
- else{
-     console.log("please enter a Valid Path!!!");
- }
+	if(folderExists){
+	    //we will code
+	    // console.log("path is valid!!!!!!");
+	    let files = fs.readdirSync(folderPath);
+	    for(let i=0;i<files.length;i++){
+	        let ext = path.extname(files[i]);
+	        let nameOfFolder = giveFolderName(ext);
+	        // console.log("Ext--",ext,"Folder--",nameOfFolder);
+	        let pathOfFolder = path.join(folderPath,nameOfFolder); 
+	        let exist = fs.existsSync(pathOfFolder);
+	        if(exist){
+	            moveFile(folderPath,pathOfFolder,files[i]);
+	        }else{
+	            fs.mkdirSync(pathOfFolder);
+	            moveFile(folderPath,pathOfFolder,files[i]);
+	        }
+            //console.log(files);
+	    }
+	}
+	else{
+	    console.log("Please Enter a Valid Path!!!!!!!");
+	}
+	
 
+	
 
+	function giveFolderName(ext){
+	    for(let key in extensions){ ///key according to our object // for each loo hai yeah
+	        let extArr = extensions[key];
+	        for(let i=0;i<extArr.length;i++){
+	            if(extArr[i] == ext){
+	                return key;
+	            }
+	        }
+	    }
+	    return 'Others'
+	}
+	
 
- function giveFolderName(ext){
-     for(let key in extensions){   // key according to our object  // for each loop hai yeah
-         let extArr = extensions[key];
-         for(let i=0; i<extArr.length; i++){
-             if(extArr[i]==ext){
-                 return key;
-            }
-        }
+	
 
-    }
-     return 'Others';
-}
+	function moveFile(folderPath,pathOfFolder,fileName){
+	    let sourcePath = path.join(folderPath,fileName);
+	    let destinationPath = path.join(pathOfFolder,fileName);
+	    fs.copyFileSync(sourcePath,destinationPath);
+	    // fs.unlinkSync(sourcePath);
+	}
+
