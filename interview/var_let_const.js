914b9a1c7862d761974ee24a3a694f03a4cc9428
Deepak Sharma <sharmadeepak200110@gmail.com>
2022-03-24 23:02:39 +0530

    var_let_const

---
+++ interview/var_let_const.js
@@ -0,0 +1,35 @@
+// //hoisting ---->declare bad me access phle.
+// console.log(a);
+// var a = 10;
+
+// console.log(a);
+// var a;          // var can re declare,reintialize and hoisting take place.
+// a=10;
+
+// console.log(a);
+// a =20;
+
+// console.log(a);
+
+// var a =50;
+// console.log(a);
+
+// let a;
+// a=10;
+// console.log(a);
+
+// a=20;
+// console.log(a);
+
+// // let a = 40;  can not re declare in let but we can in var
+// console.log(a);
+
+// const ----->canot re initialize,can't redeclare,can't be hoisted as in tdz(temporray dead zone) means undefined or can't be access
+ 
+
+const a ;   /// iss type me bhi ni hoega 
+a=10;
+console.log(a);
+
+a=20;
+console.log(a);
